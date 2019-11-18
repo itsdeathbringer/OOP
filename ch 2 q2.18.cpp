a2547c900fef31bc44561142dc23457920a1af2b
itsdeathbringer <57873137+itsdeathbringer@users.noreply.github.com>
2019-11-19 04:56:08 +0500

    Add files via upload

---
+++ ch 2 q2.18.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a, b;
+ cout << "enter two numbers";
+ cin >>a >> b; 
+ 
+ 
+ if(a == b)
+ cout<<"both are equal";
+ 
+ if(a > b)
+ cout<<"a is greater";
+ 
+ if(a < b)
+ cout<<"b is greater";
+ 
+ return 0;
+	
+	
+	
+	
+	
+	
+}
